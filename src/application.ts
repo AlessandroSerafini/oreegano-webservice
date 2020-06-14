53ff255c9f3e6e3fda16c8c699f45a6a8c26728e
Alessandro Serafini <alessandroserafini@MacBook-Pro-di-Alessandro.local>
2020-06-14 19:31:26 +0200

    minor setup

--- src/application.ts
+++ src/application.ts
@@ -1,42 +1,73 @@
 import {BootMixin} from '@loopback/boot';
 import {ApplicationConfig} from '@loopback/core';
 import {
-  RestExplorerBindings,
-  RestExplorerComponent,
+    RestExplorerBindings,
+    RestExplorerComponent,
 } from '@loopback/rest-explorer';
 import {RepositoryMixin} from '@loopback/repository';
 import {RestApplication} from '@loopback/rest';
 import {ServiceMixin} from '@loopback/service-proxy';
 import * as path from 'path';
 import {MySequence} from './sequence';
+import {MyAuthBindings, PasswordHasherBindings} from "./utils/namespaces";
+import {MyAuthAuthenticationStrategyProvider} from "./providers/MyAuthAuthenticationStrategyProvider";
+import {MyAuthMetadataProvider} from "./providers/MyAuthMetadataProvider";
+import {MyAuthActionProvider} from "./providers/MyAuthActionProvider";
+import {BcryptHasher} from "./services/hash.password.bcryptjs";
+import {AuthenticationBindings} from "@loopback/authentication";
+import {SECURITY_SCHEME_SPEC} from "./utils/enums";
+
+export interface PackageInfo {
+    name: string;
+    version: string;
+    description: string;
+}
+
+const pkg: PackageInfo = require('../package.json');
 
 export class TesiAlessandroSerafiniWsApplication extends BootMixin(
-  ServiceMixin(RepositoryMixin(RestApplication)),
+    ServiceMixin(RepositoryMixin(RestApplication)),
 ) {
-  constructor(options: ApplicationConfig = {}) {
-    super(options);
+    constructor(options: ApplicationConfig = {}) {
+        super(options);
 
-    // Set up the custom sequence
-    this.sequence(MySequence);
+        // Set up the custom sequence
+        this.sequence(MySequence);
 
-    // Set up default home page
-    this.static('/', path.join(__dirname, '../public'));
+        // Set up default home page
+        this.static('/', path.join(__dirname, '../public'));
 
-    // Customize @loopback/rest-explorer configuration here
-    this.bind(RestExplorerBindings.CONFIG).to({
-      path: '/explorer',
-    });
-    this.component(RestExplorerComponent);
 
-    this.projectRoot = __dirname;
-    // Customize @loopback/boot Booter Conventions here
-    this.bootOptions = {
-      controllers: {
-        // Customize ControllerBooter Conventions here
-        dirs: ['controllers'],
-        extensions: ['.controller.js'],
-        nested: true,
-      },
-    };
-  }
+        this.api({
+            openapi: '3.0.0',
+            info: {title: pkg.name, version: pkg.version},
+            paths: {},
+            components: {securitySchemes: SECURITY_SCHEME_SPEC},
+        });
+
+        // Customize @loopback/rest-explorer configuration here
+        this.bind(RestExplorerBindings.CONFIG).to({
+            path: '/explorer',
+        });
+        this.component(RestExplorerComponent);
+
+        this.bind(AuthenticationBindings.METADATA).toProvider(MyAuthMetadataProvider);
+        this.bind(MyAuthBindings.STRATEGY).toProvider(MyAuthAuthenticationStrategyProvider);
+        this.bind(AuthenticationBindings.AUTH_ACTION).toProvider(MyAuthActionProvider);
+
+        // // Bind bcrypt hash services
+        this.bind(PasswordHasherBindings.ROUNDS).to(10);
+        this.bind(PasswordHasherBindings.PASSWORD_HASHER).toClass(BcryptHasher);
+
+        this.projectRoot = __dirname;
+        // Customize @loopback/boot Booter Conventions here
+        this.bootOptions = {
+            controllers: {
+                // Customize ControllerBooter Conventions here
+                dirs: ['controllers'],
+                extensions: ['.controller.js'],
+                nested: true,
+            },
+        };
+    }
 }
